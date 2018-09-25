4dd93f8daddb417086addb44b2ef5a80f68377e2
Nilton Groberio <ngroberio@gmail.com>
2018-09-25 19:27:19 +0100

    +Update

--- os-adv-depl-hw-master/README.md
+++
@@ -1,1 +0,0 @@
-# os-adv-depl-hw
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo 'set log_plays to true'
+echo '>>>>>>>>>> [ SET ANSIBLE LOG ] <<<<<<<<<<<<'
 export ANSIBLE_LOG_PATH=/var/log/ansible_uninstall.log
-echo '*******************SET GUID********************'
-export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc
-echo 'GUID ==> '$GUID
-echo '*******************RUN PLAYBOOK****************'
-ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/adhoc/uninstall.yml
 
+echo '>>>>>>>>>> [ INITIATE ENV VARIABLES - EXPORT GUID ] <<<<<<<<<<<<'
+export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc
+echo 'EXPORTED GUID >>> '$GUID
+
+echo '>>>>>>>>>> [ RUN UNINSTALL PLAYBOOK ] <<<<<<<<<<<<'
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/adhoc/uninstall.yml
